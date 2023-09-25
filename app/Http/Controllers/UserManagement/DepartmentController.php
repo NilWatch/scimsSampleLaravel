a0eb171b26e6d7d7679b00f51cbfeb35912b4550
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2023-09-26 07:04:37 +0800

    update

--- app/Http/Controllers/UserManagement/DepartmentController.php
+++ app/Http/Controllers/UserManagement/DepartmentController.php
@@ -14,7 +14,7 @@ class DepartmentController extends Controller
      */
     public function index()
     {
-        //
+
     }
 
     /**
@@ -61,4 +61,7 @@ class DepartmentController extends Controller
     {
         //
     }
+
+    
+
 }
